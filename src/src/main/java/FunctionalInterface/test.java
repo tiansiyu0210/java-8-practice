c36050d6533ddcbf3f12dd832f19230675addc27
tiansiyu0210 <tiansiyu297@gmail.com>
2017-04-30 11:05:18 +0800

    first commit

---
+++ src/src/main/java/FunctionalInterface/test.java
@@ -0,0 +1,10 @@
+package FunctionalInterface;
+
+/**
+ * Created by siyu on 4/30/2017.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test.main");
+    }
+}
