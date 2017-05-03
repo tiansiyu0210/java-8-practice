37aabeabbcd3070fcf52ea51abcc78e5dd88dae0
tiansiyu0210 <tiansiyu297@gmail.com>
2017-05-03 10:37:46 +0800

    builder pattern

---
+++ src/src/main/java/BuilderPattern/AppBuilderPattern.java
@@ -0,0 +1,12 @@
+package BuilderPattern;
+
+/**
+ * Created by siyu on 5/3/2017.
+ */
+public class AppBuilderPattern {
+
+    public static void main(String[] args){
+        User tian = new User.UserBuilder("siyu", "tian").age(29).phone("123").address("1000 main st").build();
+    }
+
+}
