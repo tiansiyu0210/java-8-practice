b33bf7ec74492a55f6c88df9c20f61a2e749d354
tiansiyu0210 <tiansiyu297@gmail.com>
2017-05-11 12:20:08 +0800

    comparable

---
+++ src/src/main/java/Compare/Comparable/Duck.java
@@ -0,0 +1,48 @@
+package Compare.Comparable;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by siyu on 5/11/2017.
+ */
+public class Duck implements Comparable<Duck>{
+
+    public Duck(String name) {
+        this.name = name;
+    }
+
+    public Duck(){};
+
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public int compareTo(@NotNull Duck o) {
+        return name.compareTo(o.name);  //call String's compareTo
+    }
+    //compareTo() and equals() need consistency, so if comapreTo() returns 0 then equals() should return true;
+}
